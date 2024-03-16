f5a809ff3df1dde64c6311d60dac211b01e2a4f7
KUSH SAWLANI <124054246+KushSawlani@users.noreply.github.com>
2024-03-16 22:37:42 +0530

    feature branch merged

--- Backend/src/main/java/com/example/promact_customer_success/PromactCustomerSuccessApplication.java
+++ Backend/src/main/java/com/example/promact_customer_success/PromactCustomerSuccessApplication.java
@@ -1,4 +1,8 @@
 package com.example.promact_customer_success;
+import com.example.promact_customer_success.Entity.ApplicationUser;
+import com.example.promact_customer_success.Repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,15 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.boot.ApplicationRunner;
+
 @SpringBootApplication
 @RestController
-public class    PromactCustomerSuccessApplication {
+public class PromactCustomerSuccessApplication {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
 
         SpringApplication.run(PromactCustomerSuccessApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner applicationRunner(ApplicationUserRepository applicationUserRepository) {
+        return args -> {
+            System.out.println("Hello World from Application Runner");
+            ApplicationUser user1 = new ApplicationUser();
+            user1.setEmail("admin@gmail.com");
+            user1.setRole("Admin");
+            user1.setName("Admin");
+            applicationUserRepository.save(user1);
+        };
+    }
+    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
